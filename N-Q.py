7533a82ba7ff20734f27a57e8f0f357f6d2c6726
blobhit <79205423+blobhit@users.noreply.github.com>
2021-04-20 21:05:34 +0530

    Add files via upload
    A different solution for the N-Queens problem. :)

---
+++ N-Q.py
@@ -0,0 +1,38 @@
+print("Enter the value of N:",end='\n')
+
+n = int(input())
+A = []
+if(n<4):
+    print("No Solution!")
+else:
+    if(n%2==0):
+        p, q = 0,1
+        for i in range(n):
+            if(i<n/2):
+                A.append(q)
+                q = q+2
+            if(i>=n/2):
+                A.append(p)
+                p = p+2
+    if(n%2==1):
+        p, q = 1,0
+        for i in range(n):
+            if(i<=n/2):
+                A.append(q)
+                q = q+2
+            if(i>n/2):
+                A.append(p)
+                p = p+2
+
+
+def printboard(A):
+    for i in range(n):
+        for j in range(n):
+            if(j== A[n-1-i]):
+                print("X", end=' ')
+            else:
+                print("0", end=' ')
+        print('',end='\n')
+
+printboard(A)
+print("\n")
